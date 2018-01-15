68811a0780b6953f9531af713045298ca312051f
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2018-01-15 19:22:57 +0530

    Made changes

--- app/src/main/java/com/nqr/smsapp/MainActivity.java
+++ app/src/main/java/com/nqr/smsapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
@@ -17,10 +19,16 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.database.sqlite.*;
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
 
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     EditText address;
     TextView total;
     Button submit;
+    XYPlot plot;
     SmsManager smsManager = SmsManager.getDefault();
     private static MainActivity inst;
 
@@ -123,21 +132,76 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     String adrs = address.getText().toString();
                     refreshSmsInbox(adrs);
+                    createGraph();
+                }
+            });
+
+        }
+        public void createGraph() {
+            plot = (XYPlot) findViewById(R.id.plot);
+
+            // create a couple arrays of y-values to plot:
+            final Number[] domainLabels = {0.1, 0.2, 0.3, 0.6, 0.7, 0.8, 0.9, 0.10, 0.13, 0.14};
+            Number[] series1Numbers = {0.1, 0.4, 0.2, 0.8, 0.4, 0.16, 0.8, 0.32, 0.16, 0.64};
+            Number[] series2Numbers = {0.5, 0.2, 0.10, 0.5, 0.20, 0.10, 0.40, 0.20, 0.80, 0.40};
+
+            // turn the above arrays into XYSeries':
+            // (Y_VALS_ONLY means use the element index as the x value)
+            XYSeries series1 = new SimpleXYSeries(
+                    Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+            XYSeries series2 = new SimpleXYSeries(
+                    Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+            // create formatters to use for drawing a series using LineAndPointRenderer
+            // and configure them from xml:
+            LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+            LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+
+            // add an "dash" effect to the series2 line:
+            series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
+
+                    // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                    PixelUtils.dpToPix(20),
+                    PixelUtils.dpToPix(15)}, 0));
+
+            // just for fun, add some smoothing to the lines:
+            // see: http://androidplot.com/smooth-curves-and-androidplot/
+            series1Format.setInterpolationParams(
+                    new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+            series2Format.setInterpolationParams(
+                    new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+            // add a new series' to the xyplot:
+            plot.addSeries(series1, series1Format);
+            plot.addSeries(series2, series2Format);
+
+            plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+                @Override
+                public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                    int i = Math.round(((Number) obj).floatValue());
+                    return toAppendTo.append(domainLabels[i]);
+                }
+                @Override
+                public Object parseObject(String source, ParsePosition pos) {
+                    return null;
                 }
             });
 
         }
 
-            public void refreshSmsInbox(String adrs) {
+
+    public void refreshSmsInbox(String adrs) {
                 int i = 0;
                 float sum =0;
-
+                ArrayList<Float> tot = new ArrayList<Float>();
             ContentResolver contentResolver = getContentResolver();
             Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
             int indexBody = smsInboxCursor.getColumnIndex("body");
             int indexAddress = smsInboxCursor.getColumnIndex("address");
             if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
             arrayAdapter.clear();
+            createGraph();
             do {
                 ArrayList<Integer> amount = new ArrayList<>();
                 if (smsInboxCursor.getString(indexAddress).contains(adrs) && (smsInboxCursor.getString(indexBody).contains("Purchase") || smsInboxCursor.getString(indexBody).contains("debited"))) {
@@ -201,6 +265,7 @@ public class MainActivity extends AppCompatActivity {
                                 String c = wordlst.get(amount.get(x));
                                 c = c.replaceAll(",","");
                                 sum = sum + Float.parseFloat(c);
+                                tot.add(sum);
                             }
                         }
                         catch (Exception e){
@@ -222,7 +287,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 i++;
             } while (smsInboxCursor.moveToNext());
-
         total.setText("Total =" + String.valueOf(sum));
     }
 
